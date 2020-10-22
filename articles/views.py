098c10b09b133e8a8a1a29bcb81472331c352107
Ajay Jyothish <ajayj@qburst.com>
2020-10-22 12:46:59 +0530

    create article functionality added 1.create route is protected by authentication. 2.redirect to create page after login

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 # from django.http import HttpResponse
 
 from .models import Article
@@ -13,3 +14,7 @@ def article_list(request):
 def article_details(request,slug):
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article':article})
+
+@login_required(login_url='/accounts/login/')
+def article_create(request):
+    return render(request, "articles/article_create.html")
